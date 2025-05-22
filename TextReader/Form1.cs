01040d009e97d729cecc533dae40132c62227484
mkalme <mikelis.kalme@gmail.com>
2025-05-22 15:23:31 +0300

    Add project files.

---
+++ TextReader/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Xml;
+
+namespace TextReader
+{
+    public partial class Form1 : Form
+    {
+        private static string BasePath = "";
+        private static string FilePath = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] args = Environment.GetCommandLineArgs();
+
+            if (args.Length > 1) {
+                BasePath = args[1].Split('\n')[0];
+                FilePath = args[1].Split('\n')[1];
+
+                read();
+            }
+        }
+
+        private void read() {
+            XmlDocument document = new XmlDocument();
+            document.Load(BasePath);
+
+            richTextBox1.Text = document.SelectSingleNode(FilePath + "/text").InnerText;
+        }
+    }
+}
